ac42ca4d6a97b6786960a9b9b32380c1c916af84
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-01-18 15:04:29 -0500

    horizontal movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
-{
+{   
+    [SerializeField] float runSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
 
     
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value){
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity; 
+    }
+
+    void FlipSprite(){
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        } 
     }
 }
