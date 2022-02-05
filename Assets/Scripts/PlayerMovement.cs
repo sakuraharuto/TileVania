01404b53f938fa9ada055439dd0658bc1dedb302
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-04 19:46:11 -0500

    Prevent infinite jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     
@@ -30,6 +32,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void OnJump(InputValue value){
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            return;
+        }
         if(value.isPressed){
             myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
         }
