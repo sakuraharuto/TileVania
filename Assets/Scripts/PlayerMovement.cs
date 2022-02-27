b1a0a6fcb87c24fc3ba1f7c3076e32c38d86cd14
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-27 18:02:34 -0500

    Feet collider to prevent wall jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,14 +11,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    CapsuleCollider2D myCapsuleCollider;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
     float graviteScaleInit;
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         graviteScaleInit = myRigidbody.gravityScale;
     }
 
@@ -36,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void OnJump(InputValue value){
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
             return;
         }
         if(value.isPressed){
@@ -65,7 +67,7 @@ public class PlayerMovement : MonoBehaviour
 
     void ClimbLadder(){
 
-        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
             myRigidbody.gravityScale = graviteScaleInit;
             myAnimator.SetBool("isClimbing", false);
             return;
