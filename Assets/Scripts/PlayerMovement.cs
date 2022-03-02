17c095acaddff37d5c89f0463364bf5634ad9ccb
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-03-02 16:31:10 -0500

    Death kick

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
@@ -96,6 +97,8 @@ public class PlayerMovement : MonoBehaviour
     void Die(){
         if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies"))){
             isAlive = false;
+            myAnimator.SetTrigger("Death");
+            myRigidbody.velocity = deathKick;
             Debug.Log("YOU DIED");
         }
     }
