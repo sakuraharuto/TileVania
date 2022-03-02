43309bbcf4d1300fbd4a809d7bc404f3e2ddc99b
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-03-02 15:33:59 -0500

    Player death condition

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeetCollider;
     float graviteScaleInit;
+    bool isAlive = true;
     
     void Start()
     {
@@ -27,17 +28,27 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
+        if(!isAlive){
+            return;
+        }
         Run();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
 
     void OnMove(InputValue value){
+        if(!isAlive){
+            return;
+        }
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
 
     void OnJump(InputValue value){
+        if(!isAlive){
+            return;
+        }
         if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
             return;
         }
@@ -80,5 +91,12 @@ public class PlayerMovement : MonoBehaviour
         bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
 
         myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+
+    void Die(){
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemies"))){
+            isAlive = false;
+            Debug.Log("YOU DIED");
+        }
     }
 }
