7b64fc656e5bfdca186781d7dea04e8ac584fc75
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-03-12 23:29:19 -0500

    Coin pickup sound effect

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
-{
+{   
+    [SerializeField] AudioClip coinPickupSFX;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
